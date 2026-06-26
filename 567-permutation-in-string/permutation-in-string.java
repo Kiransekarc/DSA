b081913e28276a11f0cfd8a125e61eed955fed09
Kiransekarc <133215319+Kiransekarc@users.noreply.github.com>
2026-06-26 12:13:04 +0530

    Time: 29 ms (24.30%) | Memory: 46 MB (27.25%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> have = new HashMap<>();
+
+        for(int i=0; i<s1.length(); i++){
+            char c = s1.charAt(i);
+            if(!need.containsKey(c)) need.put(c,0);
+
+            need.put(c,need.get(c)+1);
+        }
+
+        int start = 0;
+        int end = 0;
+
+        while(end<s2.length()){
+            char c = s2.charAt(end);
+            if(!need.containsKey(c)){
+                end+=1;
+                start=end;
+                have.clear();
+                continue;
+            }
+
+            if(!have.containsKey(c)) have.put(c,0);
+
+            have.put(c, have.get(c)+1);
+
+            while(have.get(c)>need.get(c)){
+                char s = s2.charAt(start);
+                have.put(s, have.get(s)-1);
+                start++;
+            }
+            int window_size = end-start+1;
+            if(window_size==s1.length()) return true;
+            end+=1;
+        }
+        return false;
+    }
+}
