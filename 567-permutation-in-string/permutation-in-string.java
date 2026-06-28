f0eb5e188df677ddb8fc4bff2ca744af799875dd
Kiransekarc <133215319+Kiransekarc@users.noreply.github.com>
2026-06-28 19:13:15 +0530

    Time: 24 ms (29.72%) | Memory: 45.5 MB (28.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -6,34 +6,33 @@ class Solution {
         for(int i=0; i<s1.length(); i++){
             char c = s1.charAt(i);
             if(!need.containsKey(c)) need.put(c,0);
-
-            need.put(c,need.get(c)+1);
+            need.put(c, need.get(c)+1);
         }
 
         int start = 0;
         int end = 0;
-
         while(end<s2.length()){
             char c = s2.charAt(end);
             if(!need.containsKey(c)){
                 end+=1;
-                start=end;
+                start = end;
                 have.clear();
                 continue;
             }
 
-            if(!have.containsKey(c)) have.put(c,0);
-
+            if(!have.containsKey(c)) have.put(c, 0);
             have.put(c, have.get(c)+1);
 
             while(have.get(c)>need.get(c)){
                 char s = s2.charAt(start);
                 have.put(s, have.get(s)-1);
-                start++;
+                start+=1;
             }
+
             int window_size = end-start+1;
-            if(window_size==s1.length()) return true;
-            end+=1;
+            if(window_size == s1.length()) return true;
+
+            end++;
         }
         return false;
     }
