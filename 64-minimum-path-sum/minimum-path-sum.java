779f822dda63f5b0b028d8f8f22fa192a6897c7b
Kiransekarc <133215319+Kiransekarc@users.noreply.github.com>
2026-07-04 09:24:32 +0530

    Time: 5 ms (26.45%) | Memory: 50.2 MB (71.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+
         if(grid == null || grid.length == 0){
             return 0;
         }
@@ -7,19 +8,20 @@ class Solution {
         int[][] dp = new int[grid.length][grid[0].length];
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                dp[i][j]+=grid[i][j];
-    
-            if(i>0 && j>0){
-                dp[i][j]+=Math.min(dp[i-1][j],dp[i][j-1]);
-            }
-            else if(i>0){
-                dp[i][j]+=dp[i-1][j];
-            }
-            else if(j>0){
-                dp[i][j]+=dp[i][j-1];
+                if(i==0 & j==0){
+                    dp[0][0] = grid[i][j];
+                }
+                else if(i>0 && j>0){
+                    dp[i][j] = grid[i][j]+Math.min(dp[i-1][j], dp[i][j-1]);
+                }
+                else if(i>0){
+                    dp[i][j] = grid[i][j]+dp[i-1][j];
+                }
+                else if(j>0){
+                    dp[i][j] = grid[i][j]+dp[i][j-1];
+                }
             }
         }
-        }
-        return dp[grid.length-1][grid[0].length-1];
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
